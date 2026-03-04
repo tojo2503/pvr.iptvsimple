46851ba705c16295c4ac1cf8d2bc7019ee897340
tojo2503 <torbenjonsky@gmail.com>
2026-03-04 23:01:36 +0100

    fix: add missing m_streamManager member to IptvSimple

--- src/IptvSimple.h
+++ src/IptvSimple.h
@@ -16,6 +16,7 @@
 #include "iptvsimple/IConnectionListener.h"
 #include "iptvsimple/Media.h"
 #include "iptvsimple/PlaylistLoader.h"
+#include "iptvsimple/StreamManager.h"
 #include "iptvsimple/data/Channel.h"
 
 #include <atomic>
@@ -104,6 +105,7 @@ private:
   iptvsimple::PlaylistLoader m_playlistLoader{this, m_channels, m_channelGroups, m_providers, m_media, m_settings};
   iptvsimple::Epg m_epg{this, m_channels, m_media, m_settings};
   iptvsimple::CatchupController m_catchupController{m_epg, &m_mutex, m_settings};
+  iptvsimple::StreamManager m_streamManager;
   iptvsimple::ConnectionManager* connectionManager;
 
   std::atomic<bool> m_running{false};
